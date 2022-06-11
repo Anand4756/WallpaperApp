38ea505b7462062ec851bb33b4848b9d31a16570
Anandk <anand.k4756@gmail.com>
2022-06-11 09:08:55 +0530

    checking commit from another pc

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,16 @@ import 'package:wallpaper/wallpaper.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(brightness: Brightness.dark),
       home: wallpaper(),
     );
   }
 }
-
